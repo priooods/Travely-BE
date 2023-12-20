aab7d1e59d059ba11f58e9437366d1e1e4087ce8
Prio Dwi Sembodo <49172376+priooods@users.noreply.github.com>
2023-12-21 01:07:45 +0700

    [USER] users

--- server/service/service_user.js
+++ server/service/service_user.js
@@ -68,7 +68,7 @@ router.post("/login", function (req, res) {
         });
         return res.status(200).json({
           status: "success",
-          data: result[0],
+          data: result,
         });
       }
     }
