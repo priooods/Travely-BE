f65caf74b10bfc226b3383fe9cc1132288fd7955
Prio Dwi Sembodo <49172376+priooods@users.noreply.github.com>
2023-12-21 10:28:08 +0700

    [DESTINATION] updated next when call destination add

--- server/service/service_destination.js
+++ server/service/service_destination.js
@@ -15,38 +15,36 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.post("/add",upload.array('photo',5), function (req, res) {
-    const city = req.body.m_city_tabs_id;
-    const title = req.body.title;
-    const type = req.body.type;
-    const rating = req.body.rating;
-    const price = req.body.price;
-    const region = req.body.region;
-    const description = req.body.description ?? '-';
-    const facility = req.body.facility ?? '-';
+router.post("/add", upload.array("photo", 5), function (req, res, next) {
+  const city = req.body.m_city_tabs_id;
+  const title = req.body.title;
+  const type = req.body.type;
+  const rating = req.body.rating;
+  const price = req.body.price;
+  const region = req.body.region;
+  const description = req.body.description ?? "-";
+  const facility = req.body.facility ?? "-";
   const created_at = moment(Date.now()).format("YYYY-MM-DD HH:mm:ss");
   const photo = req.files;
   connection.query(
     `insert into m_destination_tabs (m_city_tabs_id,title,type,rating,price,region,description,facility,created_at) values ('${city}','${title}','${type}','${rating}','${price}','${region}','${description}','${facility}','${created_at}')`,
     function (err, result, field) {
-      if (err) throw err;
+      if (err) return res.status(500).json({ err });
       else {
         if (photo.length > 0) {
           let query = "";
           for (let i = 0; i < photo.length; i++) {
             query = query.concat(
-              `('${result.insertId}' , '${photo[i].originalname
-              }') ${i == photo.length - 1 ? "" : ","} `
+              `('${result.insertId}' , '${photo[i].originalname}') ${
+                i == photo.length - 1 ? "" : ","
+              } `
             );
           }
           connection.query(
             `insert into t_destination_attachment_tabs (m_destination_tabs_id, attachment) values ${query}`,
             function (error, res2, field) {
-              return res.status(200).json({
-                status: "success",
-                data: result,
-                photo: res,
-              });
+              if (error) return res.status(500).json(error);
+              next();
             }
           );
         }
